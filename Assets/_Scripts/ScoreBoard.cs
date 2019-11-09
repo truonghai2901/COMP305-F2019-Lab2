d74374c56774aa3452e7ed80d1ee1fc1f3be8927
Hai <truonghai29012901@gmail.com>
2019-11-09 14:11:23 -0500

    Added Scriptable Object for Score Board

---
+++ Assets/_Scripts/ScoreBoard.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "ScoreBoard", menuName ="Game/ScoreBoard")]
+[System.Serializable]
+public class ScoreBoard : ScriptableObject
+{
+    public int highScore;
+    public int lives;
+    public int score;
+}
